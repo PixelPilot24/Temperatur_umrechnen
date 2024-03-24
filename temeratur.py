ffdf4e866379f120be447c4179eff4f6acd2e7c7
Boggi <boggi.p@pm.me>
2024-03-24 17:02:26 +0100

    added Converter class, input methods and get_temp() changed,

--- temeratur.py
+++ temeratur.py
@@ -1,21 +1,57 @@
-def kelvin(celsius=0):
-    return str(celsius + 273.15)
+# TODO testen
+class Converter:
+    @staticmethod
+    def kelvin_input(temperature):
+        celsius_var = temperature - 273.15
+        fahrenheit_var = temperature * (9 / 5) - 459.15
+        print(str(celsius_var) + " °C\n" + str(fahrenheit_var) + " °F")
 
+    @staticmethod
+    def celsius_input(temperature):
+        kelvin_var = temperature + 273.15
+        fahrenheit_var = temperature * (9 / 5) + 32
+        print(str(kelvin_var) + " °K\n" + str(fahrenheit_var) + " °F")
 
-def celsius(kelvin=-273.15):
-    return kelvin - 273.15
+    @staticmethod
+    def fahrenheit_input(temperature):
+        celsius_var = (temperature - 32) * (5 / 9)
+        kelvin_var = (temperature + 459.67) * (5 / 9)
+        print(str(celsius_var) + " °C\n" + str(kelvin_var) + " °K")
 
+    @classmethod
+    def get_temp(cls):
+        print("\nMit \"beenden\" wird das Programm beendet.")
+        while True:
+            unit = input("Welche Einheit soll umgerechnet werden C, K, F: ")
+            unit_list = ["C", "c", "K", "k", "F", "f", "beenden"]
 
-def get_temp():
-    while True:
-        C = input("Gib die Temperatur in Grad Celsius ein: ")
-        try:
-            C = float(C)
-            print("Die Temperatur beträgt: " + kelvin(C) + "°K")
-            break
-        except ValueError:
-            print("Die Eingabe darf nur eine Zahl sein")
+            if unit in unit_list:
+                if unit == "beenden":
+                    break
+
+                print("Mit \"zurück\" gelangt man zur Einheitenauswahl")
+
+                while True:
+                    temperature = input("Gib die Temperatur ein: ")
+
+                    if temperature == "zurück":
+                        break
+
+                    try:
+                        temperature = float(temperature)
+
+                        if unit == "C" or unit == "c":
+                            cls.celsius_input(temperature)
+                        elif unit == "K" or unit == "k":
+                            cls.kelvin_input(temperature)
+                        else:
+                            cls.fahrenheit_input(temperature)
+                    except ValueError:
+                        print("Der eingegebene Wert muss eine Zahl sein")
+
+            else:
+                print("Die Eingabe war nicht korrekt")
 
 
 if __name__ == "__main__":
-    get_temp()
+    Converter.get_temp()
